58cb0582c0f4a698ab8e0aa14a821ddff1a0328a
Roge <herrroge@gmail.com>
2021-01-27 16:56:54 -0600

    Commit inicial

---
+++ cryptotrade/src/main/java/com/cryptotrading/cryptotrade/CryptotradeApplication.java
@@ -0,0 +1,13 @@
+package com.cryptotrading.cryptotrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptotradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptotradeApplication.class, args);
+	}
+
+}
---
+++ cryptotrade/src/test/java/com/cryptotrading/cryptotrade/CryptotradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptotrading.cryptotrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptotradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
